9094dbe12081e4e41dddb75b4cfd414f4b6146ee
Molly Wilson <molly_wilson1@outlook.com>
2021-04-03 16:23:04 -0500

    add variable lists of possible selections

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // Assignment code here
+function generatePassword() {
+  var specialChar = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+  var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerChar = [];
+  var numChar = [];
 
+  var pwdCriteria = [];
+
+  for (var i = 0; i < upper.length; i++) {
+    lower.push(upper[i].toLowerCase());
+  }
+
+  for (var i = 0; i < 10; i++) {
+    numChar.push(i.toString());
+  }
+
+  //ask how many characters 8-128
+
+  //ask upper
+
+  //ask lower
+
+  //ask numeric
+
+  //ask special
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
